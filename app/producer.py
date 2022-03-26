ec5d52ee3ef33122f2d1251a8db2d4972842de20
Chakita <chakitapesu@gmail.com>
2022-03-26 18:25:36 +0530

    Updated producer and ride sharing consumer

--- app/producer.py
+++ app/producer.py
@@ -1,5 +1,5 @@
 # Code for Producer
-from flask import Flask
+from flask import Flask,request
 import pika
 import json
 
@@ -9,23 +9,24 @@ connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
 channel.queue_declare(queue='ride-sharing')
 channel.queue_declare(queue='database')
+port = 5200
 
 @app.route('/new_ride',methods=['POST'])
 def get_data():
 	data_dict = dict()
 	if request.method=='POST':
-		pickup = request.form.get('pickup')
-		destination = request.form.get('destination')
-		time = request.form.get('time')
-		str_time = str(time)
-		cost= request.form.get('cost')
-		seats= request.form.get('seats')
-		data_dict = {'pickup':pickup,'destination':destination,'time':time,'cost':cost,'seats':seats}
+		pickup = request.args.get('pickup')
+		destination = request.args.get('destination')
+		ride_time = request.args.get('time')
+		str_time = str(ride_time)
+		cost= request.args.get('cost')
+		seats= request.args.get('seats')
+		data_dict = {'pickup':pickup,'destination':destination,'time':ride_time,'cost':cost,'seats':seats}
 		data_json=json.dumps(data_dict)
 		channel.basic_publish(exchange='',routing_key='ride-sharing',body=str_time)
 		print(str_time,"put on ride_sharing queue")
 		channel.basic_publish(exchange='',routing_key='database',body=data_json)
-		
+		return "Done"
 			
 @app.route('/new_ride_matching_consumer',methods=['POST'])
 def map():
@@ -36,10 +37,13 @@ def map():
 		mapping['Name']=consumer_id
 		mapping['ip']=ip
 		map_array.append(mapping)
+		return "done"
 
 
 
-
+if __name__ == "__main__":
+	app.run(host="0.0.0.0",port=port)
+	
 
 
 		
--- app/ride_matching_consumer.py
+++ app/ride_matching_consumer.py
@@ -6,15 +6,29 @@ import time
 
 server_ip = os.getenv('SERVER_IP',"localhost")
 consumer_id = os.getenv('CONSUMER_ID',"default")
-r= requests.post(server_ip,json={"consumer_id":cosumer_id})#Format string server ip to localhost:
-
-channel.queue_declare(queue='ride-share')
+send_to = "http://{}/new_ride_matching_consumer".format(server_ip)
+r= requests.post(send_to,json={"consumer_id":consumer_id})#Format string server ip to localhost:
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+channel.queue_declare(queue='ride-sharing')
 
 def callback(ch,method,properties,body):
 	sleep_time = int(body)
+	print(sleep_time)
 	time.sleep(sleep_time)
 	print(consumer_id)#Also print task id ?
-channel.basic_consume(queue='ride_share',auto_ack=True,on_message_callback=callback)
 
-
+channel.basic_consume(queue='ride-sharing',auto_ack=True,on_message_callback=callback)
+#connection.close()
+print(' [*] Waiting for messages. To exit press CTRL+C')
+channel.start_consuming()
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
 
