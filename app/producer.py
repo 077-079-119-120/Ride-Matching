2e5902c6f7522e50b5e825e56f8db47de4c8945d
Chakita <chakitapesu@gmail.com>
2022-03-26 15:05:25 +0530

    Ride matching consumer completed

--- app/producer.py
+++ app/producer.py
@@ -1,9 +1,10 @@
 # Code for Producer
 from flask import Flask
 import pika
+import json
 
 app = Flask(__name__)
-mapping = dict() 
+map_array = list() 
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
 channel.queue_declare(queue='ride-sharing')
@@ -11,22 +12,30 @@ channel.queue_declare(queue='database')
 
 @app.route('/new_ride',methods=['POST'])
 def get_data():
+	data_dict = dict()
 	if request.method=='POST':
 		pickup = request.form.get('pickup')
 		destination = request.form.get('destination')
 		time = request.form.get('time')
+		str_time = str(time)
 		cost= request.form.get('cost')
 		seats= request.form.get('seats')
-		#put these in the database queue
-	
+		data_dict = {'pickup':pickup,'destination':destination,'time':time,'cost':cost,'seats':seats}
+		data_json=json.dumps(data_dict)
+		channel.basic_publish(exchange='',routing_key='ride-sharing',body=str_time)
+		print(str_time,"put on ride_sharing queue")
+		channel.basic_publish(exchange='',routing_key='database',body=data_json)
+		
+			
 @app.route('/new_ride_matching_consumer',methods=['POST'])
 def map():
+	mapping = dict()
 	if request.method=='POST':
 		ip = request.remote_addr
 		consumer_id = request.form.get('consumer_id')
 		mapping['Name']=consumer_id
 		mapping['ip']=ip
-		#put these in the ride_sharing queue
+		map_array.append(mapping)
 
 
 
--- app/ride_matching_consumer.py
+++ app/ride_matching_consumer.py
@@ -1,1 +1,20 @@
 # Code for Ride Matching Consumer
+import os 
+import requests
+import pika
+import time
+
+server_ip = os.getenv('SERVER_IP',"localhost")
+consumer_id = os.getenv('CONSUMER_ID',"default")
+r= requests.post(server_ip,json={"consumer_id":cosumer_id})#Format string server ip to localhost:
+
+channel.queue_declare(queue='ride-share')
+
+def callback(ch,method,properties,body):
+	sleep_time = int(body)
+	time.sleep(sleep_time)
+	print(consumer_id)#Also print task id ?
+channel.basic_consume(queue='ride_share',auto_ack=True,on_message_callback=callback)
+
+
+
